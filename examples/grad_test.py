97581f5321f70989597287334d532dbbb828ebac
Chris Bay <christopher.j.bay@gmail.com>
2020-01-03 15:46:45 -0700

    trying something else

--- examples/grad_test.py
+++ examples/grad_test.py
@@ -5,14 +5,23 @@ from autograd import grad    # the only autograd function you may ever need
 # import matplotlib.pyplot as plt
 import floris.tools as wfct
 
+def grad_func(yaw_angles, fi):
+    fi.calculate_wake(yaw_angles=yaw_angles)
+
+    return fi.get_farm_power()
+
 fi = wfct.floris_interface.FlorisInterface("example_input.json")
 
-yaw_angles = [1., 10.]
+yaw_angles = [0.1, 0.1, 0.1]
 
-power = fi.get_farm_power_for_yaw_angle(yaw_angles=yaw_angles)
+# power = fi.get_farm_power_for_yaw_angle(yaw_angles=yaw_angles)
 
-yaw_grad = grad(fi.get_farm_power_for_yaw_angle)
+# yaw_grad = grad(fi.get_farm_power_for_yaw_angle)
 
-print('innnnnnnnnnnnn')
-print(yaw_grad(yaw_angles))
+yaw_grad = grad(grad_func)
+
+print('--------------------------')
+print('starting grad calculation')
+print('--------------------------')
+print(yaw_grad(yaw_angles, fi))
 
