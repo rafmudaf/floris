7e0f0ad2daab25104028069a751a67c179b944ca
Rob Hammond <13874373+RHammond2@users.noreply.github.com>
2021-12-13 10:38:50 -0700

    add comments and test parameter setting

--- tests/floris_interface_test.py
+++ tests/floris_interface_test.py
@@ -138,6 +138,7 @@ def test_get_model_parameters():
     # TODO: Add in the turbulence model component once implemented!
     fi = FlorisInterface(configuration=JSON_INPUT)
 
+    # Test to check that all parameters are returned
     correct_parameters = {
         "wake_deflection_parameters": {"ad": 0.0, "bd": 0.0, "kd": 0.05},
         "wake_velocity_parameters": {"we": 0.05},
@@ -145,21 +146,37 @@ def test_get_model_parameters():
     parameters = fi.get_model_parameters(turbulence_model=False)
     assert parameters == correct_parameters
 
+    # Test to check that only deflection parameters are returned
     correct_parameters = {"wake_deflection_parameters": {"ad": 0.0, "bd": 0.0, "kd": 0.05}}
     parameters = fi.get_model_parameters(turbulence_model=False, wake_velocity_model=False)
     assert parameters == correct_parameters
 
+    # Test to check that only velocity models are returned
     correct_parameters = {"wake_velocity_parameters": {"we": 0.05}}
     parameters = fi.get_model_parameters(turbulence_model=False, wake_deflection_model=False)
     assert parameters == correct_parameters
 
+    # Check that oly "ad" and "kd" values are returned
     correct_parameters = {"wake_deflection_parameters": {"ad": 0.0, "kd": 0.05}, "wake_velocity_parameters": {}}
     parameters = fi.get_model_parameters(turbulence_model=False, params=["ad", "kd"])
     assert parameters == correct_parameters
 
 
-def test_set_model_parameters():  # TODO
-    pass
+def test_set_model_parameters():
+    # TODO: Add in the turbulence model component once implemented!
+    fi = FlorisInterface(configuration=JSON_INPUT)
+
+    correct_parameters = {
+        "wake_deflection_parameters": {"ad": 0.1, "bd": 0.0, "kd": 0.05},
+        "wake_velocity_parameters": {"we": 0.22},
+    }
+    update_parameters = {
+        "Wake Deflection Parameters": {"ad": 0.1},  # Check v2 naming convention
+        "wake velocity_Parameters": {"we": 0.22},  # Check mixed naming convention
+    }
+    fi.set_model_parameters(params=update_parameters)
+    parameters = fi.get_model_parameters(turbulence_model=False)
+    assert parameters == correct_parameters
 
 
 def test_vis_layout():
