d37313a235fb522a8feccd5647d7ccd1cb6b47e0
Rafael M Mudafort <rafmudaf@gmail.com>
2023-12-07 14:13:50 -0600

    Reset formatting

--- tests/utilities_unit_test.py
+++ tests/utilities_unit_test.py
@@ -90,7 +90,8 @@ def test_rotate_coordinates_rel_west():
     # For 270, the coordinates should not change.
     wind_directions = np.array([270.0])
     x_rotated, y_rotated, z_rotated, _, _ = rotate_coordinates_rel_west(
-        wind_directions, coordinates
+        wind_directions,
+        coordinates
     )
 
     # Test that x_rotated has 2 dimensions
@@ -112,12 +113,14 @@ def test_rotate_coordinates_rel_west():
     # conftest change.
     wind_directions = np.array([360.0])
     x_rotated, y_rotated, z_rotated, _, _ = rotate_coordinates_rel_west(
-        wind_directions, coordinates
+        wind_directions,
+        coordinates
     )
     np.testing.assert_almost_equal(Y_COORDS, x_rotated[0] - np.min(x_rotated[0]))
     np.testing.assert_almost_equal(X_COORDS, y_rotated[0] - np.min(y_rotated[0]))
     np.testing.assert_almost_equal(
-        Z_COORDS + np.min(Z_COORDS), z_rotated[0] + np.min(z_rotated[0])
+        Z_COORDS + np.min(Z_COORDS),
+        z_rotated[0] + np.min(z_rotated[0])
     )
 
     wind_directions = np.array([90.0])
